95129988a4014c0219a2c60c1913e5ed4d587b34
sujitharajendran <sujikavi66@gmail.com>
2017-06-21 12:22:50 +0530

    Add files via upload

---
+++ Stringreverse.java
@@ -0,0 +1,14 @@
+package wipro;
+
+public class Stringreverse {
+
+	public static void main(String[] args) {
+		String s1="sujitha";
+		StringBuffer s2=new StringBuffer(s1);
+s2=s2.reverse();
+String s=s2.toString();
+System.out.println(s);
+
+	}
+
+}
